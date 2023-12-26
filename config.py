2c475344789d1a8c3f9ab5292e39209ebb9a73e4
JoelK02 <75214236+JoelK02@users.noreply.github.com>
2023-12-26 21:00:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.c5wikg2owt2u.us-east-1.rds.amazonaws.com"
+customhost = "assignment-database.c5wikg2owt2u.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
