879d0c1cb7c6766ffb58a4bc3c26d4a026bf9705
JoelK02 <75214236+JoelK02@users.noreply.github.com>
2023-12-26 21:06:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "assignment-database.c5wikg2owt2u.us-east-1.rds.amazonaws.com"
 customuser = "joelkwoh"
 custompass = "021112130735"
-customdb = "employee"
+customdb = "assignment-database"
 custombucket = "joelkwoh-employee"
-customregion = "us-east-1"
+customregion = "us-east-1d"
 
